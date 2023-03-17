fb1cbd72e21d2a11a9070ed8f9cbcd4e98b96070
waqas rafiq <wm66179@gmail.com>
2023-03-17 18:16:53 +0500

    Practice

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// function createMatrix() {
-//     var rows = document.getElementById("rows").value;
-//     var columns = document.getElementById("columns").value;
-//     var matrixHTML = "<table>";
-//     for (var i = 0; i < rows; i++) {
-//         matrixHTML += "<tr>";
-//         for (var j = 0; j < columns; j++) {
-//             matrixHTML += "<td><input type='number' id='a" + i + j + "'></td>";
-//         }
-//         matrixHTML += "</tr>";
-//     }
-//     matrixHTML += "</table>";
-//     document.getElementById("matrix").innerHTML = matrixHTML;
-// }
 
-// function calculate() {
-//     var rows = document.getElementById("rows").value;
-//     var columns = document.getElementById("columns").value;
-//     var matrix = [];
-//     for (var i = 0; i < rows; i++) {
-//         var row = [];
-//         for (var j = 0; j < columns; j++) {
-//             row.push(parseFloat(document.getElementById("a" + i + j).value));
-//         }
-//         matrix.push(row);
-//     }
-//     document.getElementById("result").innerHTML = "Result: ";
-// }
+function calculateMetrix(operation) {
+
+    let val1_1 = parseInt(document.getElementById("val1_1").value);
+    let val1_2 = parseInt(document.getElementById("val1_2").value);
+    let val1_3 = parseInt(document.getElementById("val1_3").value);
+    let val1_4 = parseInt(document.getElementById("val1_4").value);
+
+    let FirstValues = [val1_1, val1_2, val1_3, val1_4]
+    
+
+    let val2_1 = parseInt(document.getElementById("val2_1").value);
+    let val2_2 = parseInt(document.getElementById("val2_2").value);
+    let val2_3 = parseInt(document.getElementById("val2_3").value);
+    let val2_4 = parseInt(document.getElementById("val2_4").value);
+    
+    let SecondValues = [val2_1, val2_2, val2_3, val2_4]
+
+    res1 = FirstValues[0] + SecondValues[0]
+    res2 = FirstValues[1] + SecondValues[1]
+    res3 = FirstValues[2] + SecondValues[2]
+    res4 = FirstValues[3] + SecondValues[3]
+
+    document.querySelector("#res2").innerHTML = res2
+    document.querySelector("#res1").innerHTML = res1
+    document.querySelector("#res3").innerHTML = res3
+    document.querySelector("#res4").innerHTML = res4
+}
